3f01d9aa5149b8495cda61d12a6eacfac6d3ea70
GustavoJBM <gustavo.jaroski@gmail.com>
2026-04-08 14:38:32 +0200

    Delete include directory

--- include/mqtt_handler.h
+++
@@ -1,7 +0,0 @@
-#ifndef MQTT_HANDLER_H
-#define MQTT_HANDLER_H
-
-void setup_mqtt();
-void loop_mqtt();
-
-#endif
--- include/wifi_handler.h
+++
@@ -1,7 +0,0 @@
-#ifndef WIFI_HANDLER_H
-#define WIFI_HANDLER_H
-
-void setup_wifi();
-bool is_wifi_connected();
-
-#endif
